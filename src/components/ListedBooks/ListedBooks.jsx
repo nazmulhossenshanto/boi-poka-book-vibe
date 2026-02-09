1365e015c20ad4137df4f18c4e49a35a6a81a55c
nazmulhossenshanto <https://github.com/nazmulhossenshanto>
2026-02-09 17:32:58 +0600

    added sorted funtionality

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -7,6 +7,7 @@ import BookCard from "../BookCard/BookCard";
 
 const ListedBooks = () => {
   const [bookReadList, setBookReadList] = useState([]);
+  const [sort, setSort] = useState("");
   const allBooks = useLoaderData();
   useEffect(() => {
     const storedReadList = getStoredReadList();
@@ -16,8 +17,39 @@ const ListedBooks = () => {
     );
     setBookReadList(readList);
   }, [allBooks]);
+  const handleSort = (sortType) => {
+    setSort(sortType);
+    if (sortType == "Pages") {
+      const sortedReadList = [
+        ...bookReadList.sort((a, b) => a.totalPages - b.totalPages),
+      ];
+      setBookReadList(sortedReadList);
+    }
+    if (sortType == "Ratings") {
+      const sortedReadList = [
+        ...bookReadList.sort((a, b) => a.rating - b.rating),
+      ];
+      setBookReadList(sortedReadList);
+    }
+  };
   return (
     <div>
+      <div className="dropdown flex justify-center my-3">
+        <div tabIndex={0} role="button" className="btn m-1">
+          {sort ? `sort by : ${sort}` : "sort by"}
+        </div>
+        <ul
+          tabIndex="-1"
+          className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+        >
+          <li onClick={() => handleSort("Ratings")}>
+            <a>Ratings</a>
+          </li>
+          <li onClick={() => handleSort("Pages")}>
+            <a>Number of pages</a>
+          </li>
+        </ul>
+      </div>
       <Tabs>
         <TabList>
           <Tab>ReadList </Tab>
@@ -26,12 +58,10 @@ const ListedBooks = () => {
 
         <TabPanel>
           <h2>Books I read {bookReadList.length}</h2>
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-5'>
-            {
-          bookReadList.map((book) => (
-            <BookCard key={book.bookId} book={book} />
-          ))
-          }
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-5">
+            {bookReadList.map((book) => (
+              <BookCard key={book.bookId} book={book} />
+            ))}
           </div>
         </TabPanel>
         <TabPanel>
